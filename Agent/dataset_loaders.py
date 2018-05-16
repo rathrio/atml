81231edd7c590c7aa1575788638a5051f628ccd3
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-16 19:37:47 +0200

    Custom torch data loader for albums

--- Agent/dataset_loaders.py
+++ Agent/dataset_loaders.py
@@ -1,4 +1,6 @@
 from torch.utils.data import Dataset
+import numpy as np
+import csv
 
 
 class MusicDataset(Dataset):
@@ -15,18 +17,34 @@ class MusicDataset(Dataset):
         self.path_to_metadata = path_to_metadata
         self.path_to_vggfeatures = path_to_vggfeatures
 
+        self.albums = []
 
+        with open("data/metadata.csv") as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            next(reader, None)  # skip headers
+            for row in reader:
+                self.albums.append(row)
+
+        print(f'Loaded {len(self.albums)} albums')
+
+        self.vggfeatures = np.load(path_to_vggfeatures)
+        print(f'Loaded {self.vggfeatures.shape[0]} vggfeatures')
 
     def __getitem__(self, index):
         #how to get a single item
         #What is needed is music vgg features extracted, artist name and genre
 
-        return #vggfeatures, artist_name, genre
+        row = self.albums[index]
+        artist_name = row[2]
+        genre = row[5].split("|")[0]
+
+        return self.vggfeatures[index], artist_name, genre
 
     def __len__(self):
-        return # how to get length of the dataset
+        return self.vggfeatures.shape[0] # how to get length of the dataset
 
-dataset = MusicDataset('../data/metadata.csv', 'foobar')
+# dataset = MusicDataset('./data/metadata.csv', './data/music_alb.npy')
+# dataset.__getitem__(12347)
 
 
 
