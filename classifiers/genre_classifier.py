d7279e08463af0f03c60821df090f2b4a119ca9f
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-17 22:06:24 +0200

    Move metadata wrapper to separate file so that it can be reused

--- classifiers/genre_classifier.py
+++ classifiers/genre_classifier.py
@@ -2,51 +2,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-import csv
+from metadata import Metadata
+
 import IPython
-
-class Metadata:
-    genres = ["Electronic",
-              "Rock",
-              "Pop",
-              "Jazz",
-              "Hip Hop",
-              "Funk / Soul",
-              "Folk, World, & Country",
-              "Classical",
-              "Reggae",
-              "Non-Music",
-              "Latin",
-              "Blues",
-              "Children's",
-              "Stage & Screen",
-              "Brass & Military"]
-
-    genre_count = len(genres)
-
-    def __init__(self, path_to_metadata):
-        self.path_to_metadata = path_to_metadata
-        self.albums = []
-
-        with open(path_to_metadata) as csvfile:
-            reader = csv.reader(csvfile, delimiter=';')
-            next(reader, None)  # skip headers
-            for row in reader:
-                self.albums.append(row)
-
-        print(f'Loaded {len(self.albums)} albums')
-
-    def genre_tensor(self, genre):
-        index = self.genres.index(genre)
-        t = torch.Tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        t[index] = 1
-        return t
-
-    def album(self, index):
-        return self.albums[index]
-
-    def genre(self, index):
-        return self.album(index)[5].split("|")[0]
 
 
 class GenreClassifier(nn.Module):
@@ -62,6 +20,7 @@ class GenreClassifier(nn.Module):
         out = self.fc2(out)
         return self.sm(out)
 
+
 metadata = Metadata('./data/metadata.csv')
 
 input_size = 4096
@@ -72,11 +31,8 @@ learning_rate = 0.0001
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
-
 features = Variable(torch.randn(input_size))
 out = net(features)
-
-IPython.embed()
 
 
 for epoch in range(2):
