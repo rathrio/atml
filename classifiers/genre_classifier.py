47edc259e7af76aeae1d5a5685c3b52857c6bac0
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-18 14:52:56 +0200

    Add train loop

--- classifiers/genre_classifier.py
+++ classifiers/genre_classifier.py
@@ -8,6 +8,9 @@ from metadata import Metadata
 
 import IPython
 
+metadata_path = "./data/metadata.csv"
+features_path = "./data/music_alb.npy"
+
 
 class GenreClassifier(nn.Module):
     def __init__(self, input_size, hidden_size, genre_count):
@@ -19,25 +22,44 @@ class GenreClassifier(nn.Module):
     def forward(self, features):
         out = self.fc1(features)
         out = self.fc2(out)
-        return F.softmax(out)
+        return F.softmax(out, dim=-1)
 
 
-metadata = Metadata('./data/metadata.csv')
+metadata = Metadata(metadata_path)
 albums = metadata.albums
-all_features = np.load('./data/music_alb.npy')
+all_features = np.load(features_path)
+print(f'Loaded {len(all_features)} features')
+
+assert len(albums) == len(all_features)
 
 input_size = 4096
-hidden_size = 1000
+hidden_size = 512
 genre_count = metadata.genre_count
 net = GenreClassifier(input_size, hidden_size, genre_count)
 
-learning_rate = 0.0001
+learning_rate = 1e-3
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+batch_size = 1000
 
-features = Variable(torch.randn(input_size))
-out = net(features)
+for epoch in range(2):
 
+    running_loss = 0.0
+    for i, album in enumerate(albums, 0):
+        features = Variable(torch.from_numpy(all_features[i]))
+        genre = Variable(metadata.genre_tensor(album.genre))
 
-for epoch in range(3):
-    pass
+        optimizer.zero_grad()
+
+        out = net(features)
+        loss = criterion(out, genre)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % batch_size == (batch_size - 1):
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / batch_size))
+            running_loss = 0.0
+
+print('Done!')
