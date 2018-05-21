d6f555aba5f91ab480e9abf59f725beab590934d
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-05-22 01:11:49 +0200

    Cleanup

--- utils/genre_browser/browser.rb
+++ utils/genre_browser/browser.rb
@@ -15,17 +15,23 @@ end
 get '/' do
   albums = Album
 
-  if params[:genre].nil? && params[:artist].nil?
-    return "No filters applied"
-  end
+  # if params[:genre].nil? && params[:artist].nil? && params[:style].nil?
+  #   return "No filters applied"
+  # end
 
-  if params[:genre]
-    albums = albums.where(genre: params[:genre])
-  end
+  # if params[:genre]
+  #   albums = albums.where(genre: params[:genre])
+  # end
 
-  if params[:artist]
-    albums = albums.where(Sequel.like(:artist, "%#{params[:artist]}%"))
-  end
+  # if params[:style]
+  #   albums = albums.where(Sequel.like(:styles, "%#{params[:style]}%"))
+  # end
+
+  # if params[:artist]
+  #   albums = albums.where(Sequel.like(:artist, "%#{params[:artist]}%"))
+  # end
+
+  albums = albums.where(genre: "Folk, World, & Country")
 
   erb :index, locals: { albums: albums.to_a.sample(80) }
 end
